a8e689cec6e93adf086990929703073ea8512ea2
Felix Langfeldt <felix@neverwoods.com>
2019-06-08 16:15:03 -0400

    fixes PowerPallet

--- src/PowerPallet.js
+++ src/PowerPallet.js
@@ -1,10 +1,10 @@
 function PowerPallet(options) {
-    Eatable.call(this, {options});
-    this.type = "Celery";
+    Eatable.call(this, options);
+    this.type = "PowerPallet";
     this.value = 50;
 }
 PowerPallet.prototype = Object.create(Eatable.prototype);
-PowerPallet.prototype.constructor = Celery;
+PowerPallet.prototype.constructor = PowerPallet;
 
 /**
  * Render
@@ -15,8 +15,8 @@ PowerPallet.prototype.render = function () {
     this.icon.src = 'images/celery.svg';
     this.icon.style.width = '16px';
     this.icon.style.height = '16px';
-    let centerX = labyrinth.positionToPixel(this.x) + this.icon.width / 2;
-    let centerY = labyrinth.positionToPixel(this.y) + this.icon.height / 2;
+    let centerX = labyrinth.positionToPixel(this.x) - (16 / 2);
+    let centerY = labyrinth.positionToPixel(this.y) - (16 / 2);
     this.elementInstance.style.position = 'absolute';
     this.elementInstance.style.left = centerX + 'px';
     this.elementInstance.style.top = centerY + 'px';
