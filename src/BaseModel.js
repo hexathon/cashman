991258f7a2be453bf1c6c145e54841b16cba035a
Kyurchiev,Andrea A.G <andrea@celerypayroll.com>
2019-06-08 15:32:18 -0400

    refactors the basemodel

--- src/BaseModel.js
+++ src/BaseModel.js
@@ -1,40 +1,34 @@
- function BaseModel(x, y, icon, container){
-	this.x = x;
-	this.y = y;
-	this.icon = icon;
-	this.container = container;
-	let getPosition= function() {
-		return {x:this.x, y:this.y};
-	};
+ function BaseModel(options) {
+	 this.x = options.x;
+	 this.y = options.y;
+	 this.container = options.container;
+ }
 
-	let move = function(x,y) {
-		if (window.labyrinth.canIGoThere(x, y)) {
-			this.x = x;
-			this.y = y;
-			return true;
-		}
-		return false;
-	};
+ BaseModel.prototype.getPosition = function () {
+	 return {x: this.x, y: this.y};
+ };
 
-	let moveUp = function() {
-		move(this.x, (this.y--));
-	};
+ BaseModel.prototype.move = function(x,y) {
+	if (window.labyrinth.canIGoThere(x, y)) {
+		this.x = x;
+		this.y = y;
+		return true;
+	}
+	return false;
+ };
 
-	let moveDown = function() {
-		move(this.x, (this.y++));
-	};
+ BaseModel.prototype.moveUp = function() {
+		this.move(this.x, (this.y--));
+ };
 
-	let moveLeft = function() {
-		move((this.x--), this.y);
-	};
+ BaseModel.prototype.moveDown = function() {
+		this.move(this.x, (this.y++));
+ };
 
-	let moveRight = function() {
-		move((this.x++), this.y);
-	};
+ BaseModel.prototype.moveLeft = function() {
+		this.move((this.x--), this.y);
+ };
 
-	let render = function() {
-	};
-
-	return {moveUp:moveUp,moveDown:moveDown, moveLeft:moveLeft, moveRight:moveRight, render:render};
-
-}
+ BaseModel.prototype.moveRight = function() {
+		this.move((this.x++), this.y);
+ };
