bdfbc15bb20bdf7313f4a940ee292c5a4d4412cb
Kyurchiev,Andrea A.G <andrea@celerypayroll.com>
2019-06-08 15:41:48 -0400

    refactors the celer and powerpelet model

--- src/Celery.js
+++ src/Celery.js
@@ -1,9 +1,16 @@
-import Eatable from './Eatable';
-
-class Celery extends Eatable {
-    constructor(x, y) {
-        super(100, x, y, 'Celery');
-    }
+function Celery(x, y, container) {
+    Eatable.call(this, x, y, container);
+    this.type = "Celery";
+    this.value = 100;
 }
+Celery.prototype = Object.create(Eatable.prototype);
+Celery.prototype.constructor = Celery;
 
-export default Celery;
+/**
+ * Render
+ */
+Celery.prototype.render = function () {
+    this.elementInstance = document.createElement('div');
+    this.elementInstance.style = "position:absolute;width:8px;height:8px;background:orange;left:" + this.x + "px;top:" + this.y + "px;";
+    this.container.appendChild(this.elementInstance);
+};
--- src/PowerPallet.js
+++ src/PowerPallet.js
@@ -1,9 +1,16 @@
-import Eatable from './Eatable';
-
-class PowerPallet extends Eatable {
-    constructor(x, y) {
-        super(50, x, y, 'PowerPallet');
-    }
+function Celery(x, y, container) {
+    Eatable.call(this, x, y, container);
+    this.type = "Celery";
+    this.value = 50;
 }
+Celery.prototype = Object.create(Eatable.prototype);
+Celery.prototype.constructor = Celery;
 
-export default PowerPallet;
+/**
+ * Render
+ */
+CashDot.prototype.render = function () {
+    this.elementInstance = document.createElement('div');
+    this.elementInstance.style = "position:absolute;width:16px;height:16px;background:orange;left:" + this.x + "px;top:" + this.y + "px;";
+    this.container.appendChild(this.elementInstance);
+};
