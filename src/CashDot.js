a0246bc54f0d756c22e02c19180417b7e0c6e356
Robin van Baalen <robin@cattlea.com>
2019-06-09 10:34:27 -0400

    feat: variable size cookies

--- src/CashDot.js
+++ src/CashDot.js
@@ -1,7 +1,8 @@
 function CashDot(options) {
     Eatable.call(this, options);
     this.type = "CashDot";
-    this.value = 10;
+    this.value = options.value || 10;
+    this.size = options.size || 8;
 }
 CashDot.prototype = Object.create(Eatable.prototype);
 CashDot.prototype.constructor = CashDot;
@@ -12,16 +13,16 @@ CashDot.prototype.constructor = CashDot;
 CashDot.prototype.render = function () {
     this.elementInstance = document.createElement('div');
 
-    let centerX = labyrinth.positionToPixel(this.x) - 4;
-    let centerY = labyrinth.positionToPixel(this.y) - 4;
+    let centerX = labyrinth.positionToPixel(this.x) - (this.size / 2);
+    let centerY = labyrinth.positionToPixel(this.y) - (this.size / 2);
 
     this.elementInstance.style.position = 'absolute';
-    this.elementInstance.style.width = '8px';
-    this.elementInstance.style.height = '8px';
+    this.elementInstance.style.width = this.size + 'px';
+    this.elementInstance.style.height = this.size + 'px';
     this.elementInstance.style.left = centerX + 'px';
     this.elementInstance.style.top = centerY + 'px';
     this.elementInstance.style.background = 'orange';
-    this.elementInstance.style.borderRadius = '4px';
+    this.elementInstance.style.borderRadius = (this.size / 2) + 'px';
 
     this.container.appendChild(this.elementInstance);
 };
