8305b57e030c1fdbf42fedab47feba7c00414a20
Tom Ummels <tom@tomsplanner.com>
2019-06-09 11:28:05 -0400

    sounds.js version 1.01

--- src/sound.js
+++ src/sound.js
@@ -1,21 +1,36 @@
 var soundmanager = {
-    sounds:{},
+    sounds:{
+        silence : {pause:function(){;},fastSeek:function(){;},loop:false},
+        intro : new Audio("sounds/pacman_beginning.wav"),
+        chomp : new Audio("sounds/pacman_chomp.wav")
+    },
+    lastplayed:"silence",
     init:function(){
-        this.sounds.intro = new Audio("sounds/pacman_beginning.wav");
-        this.sounds.chomp = new Audio("sounds/pacman_chomp.wav");    
-        this.sounds.chomp.loop=true;
+        for(sound in this.sounds){
+            this.sounds[sound].volume=0.1;
+        }
         return this;
     },
-    active: null,
-    stop: function (soundname) {
-        this.sounds[soundname].stop(); 
+    stop: function (soundname,letitfinish) {
+        soundname = soundname ||this.lastplayed;
+        this.sounds[soundname].loop=false;
+        if(!letitfinish)this.sounds[soundname].pause(); 
+        this.lastplayed="silence";
     },
-    start: function (soundname) {
-        this.sounds[soundname].play();
+    start: function (soundname,shouldloop) {
+        if(!(this.lastplayed===soundname && this.sounds[this.lastplayed].loop)){
+            this.stop();
+            this.sounds[soundname].currentTime=0;
+            this.sounds[soundname].loop=shouldloop;
+            this.sounds[soundname].play();
+            this.lastplayed=soundname;
+        }
     }
 }.init();
 
-/*window.addEventListener('cashman.execute.move', (event) => {
-      soundmanager.start("chomp");
-    }, true);
-    */
+window.addEventListener('cashman.move', (event) => {
+      soundmanager.start("chomp",true);
+}, true);
+window.addEventListener('cashman.stop', (event) => {
+    soundmanager.stop("chomp",true);
+}, true);
